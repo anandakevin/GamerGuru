7f3edc316c2e5b81923e218b0bda8cdcb9f1ddff
Ananda Kevin <anandakevinsariputra@gmail.com>
2019-04-29 15:30:56 +0700

    Initial commit

---
+++ GamerGuru/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GamerGuru
+//
+//  Created by Ananda Kevin Refaldo Sariputra on 29/04/19.
+//  Copyright © 2019 Ananda Kevin Refaldo Sariputra. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GamerGuruTests/GamerGuruTests.swift
@@ -0,0 +1,34 @@
+//
+//  GamerGuruTests.swift
+//  GamerGuruTests
+//
+//  Created by Ananda Kevin Refaldo Sariputra on 29/04/19.
+//  Copyright © 2019 Ananda Kevin Refaldo Sariputra. All rights reserved.
+//
+
+import XCTest
+@testable import GamerGuru
+
+class GamerGuruTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
