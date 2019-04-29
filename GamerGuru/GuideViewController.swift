baf5ff4f90e3a434d7bedd2e6fc18687c1b72e8f
Ananda Kevin <anandakevinsariputra@gmail.com>
2019-04-29 16:43:44 +0700

    tes update to kevinBranch

--- GamerGuru/GuideViewController.swift
+++ GamerGuru/GuideViewController.swift
@@ -12,6 +12,7 @@ class GuideViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Tes Update")
         // Do any additional setup after loading the view.
     }
 
