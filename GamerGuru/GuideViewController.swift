79626490b67917f93b89f00396123a817b013adf
Ananda Kevin <anandakevinsariputra@gmail.com>
2019-04-29 16:45:03 +0700

    test revert update on kevinBranch

--- GamerGuru/GuideViewController.swift
+++ GamerGuru/GuideViewController.swift
@@ -12,7 +12,6 @@ class GuideViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Tes Update")
         // Do any additional setup after loading the view.
     }
 
