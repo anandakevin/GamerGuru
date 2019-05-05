69a85384c16a4d81656e9d9ef14ff008f72e90ac
Chia Pingky <pingky99@gmail.com>
2019-05-05 19:38:32 +0700

    beres sampe topic map
    tinggal view gambarnya tapi gambarnya blom ada jadi besok aja

---
+++ GamerGuru/GamerGuru/GuideContentViewController.swift
@@ -0,0 +1,51 @@
+//
+//  GuideContentViewController.swift
+//  GamerGuru
+//
+//  Created by Chia Pingky on 05/05/19.
+//  Copyright © 2019 Chia Pingky. All rights reserved.
+//
+
+import UIKit
+
+class GuideContentViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var contentTable: UITableView!
+    let topicTitle:[String] = ["Map", "Creep and Jungle", "Heroes", "Items", "Item Build for Heroes"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        searchBar.backgroundImage = UIImage()
+        searchBar.backgroundColor = #colorLiteral(red: 0.9763886333, green: 0.9765250087, blue: 0.9763454795, alpha: 1)
+    }
+}
+
+extension GuideContentViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return topicTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = (contentTable.dequeueReusableCell(withIdentifier: "contentCell", for: indexPath) as? GuideTopicTableViewCell)!
+        cell.cellTitle.text = topicTitle[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row != 0 {
+            let alert = UIAlertController(title: "Content In Progress", message: "We are still working on this topic, but it will be available soon", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else{
+            performSegue(withIdentifier: "topicSelected", sender: self)
+        }
+        contentTable.deselectRow(at: indexPath, animated: true)
+    }
+}
---
+++ GamerGuru/GamerGuru/GuideViewController.swift
@@ -0,0 +1,48 @@
+//
+//  GuideViewController.swift
+//  GamerGuru
+//
+//  Created by Chia Pingky on 05/05/19.
+//  Copyright © 2019 Chia Pingky. All rights reserved.
+//
+
+import UIKit
+
+class GuideViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var gameList: UICollectionView!
+    let gameLogo:[String] = ["Dota 2", "CS-GO", "PUBG", "Fortnite", "APEX", "Crossfire"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchBar.setBackgroundImage(UIImage(), for: .top, barMetrics: .default)
+        searchBar.backgroundColor = #colorLiteral(red: 0.9763886333, green: 0.9765250087, blue: 0.9763454795, alpha: 1)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+    }
+        
+}
+
+extension GuideViewController : UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return gameLogo.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = (gameList.dequeueReusableCell(withReuseIdentifier: "logoCell", for: indexPath) as? GameCollectionViewCell)!
+        cell.gameLogo.image = UIImage(named: gameLogo[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if indexPath.row != 0 {
+            let alert = UIAlertController(title: "Content Coming Soon", message: "Content not available yet, but will be available soon.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else {
+            performSegue(withIdentifier: "gameSelected", sender: self)
+            
+        }
+    }
+}
